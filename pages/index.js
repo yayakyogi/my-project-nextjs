8039ff592f903275521244667640cd50a24327cd
yayaktaka@gmail.com <yayaktaka@gmail.com>
2021-01-15 15:49:48 +0700

    ubah nama title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container bg-gray-100">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>My Project</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
