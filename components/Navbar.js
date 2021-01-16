bc3a7c068d9e2d50f822021c83ade3b785b5b795
yayaktaka@gmail.com <yayaktaka@gmail.com>
2021-01-16 20:35:46 +0700

    ubah jarak button dark mode

--- components/Navbar.js
+++ components/Navbar.js
@@ -26,7 +26,7 @@ export default function Nav(){
                         <path fillRule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clipRule="evenodd" />
                     </svg>
                     <div className="bg-indigo-400 dark:bg-indigo-700 flex justify-between items-center rounded-full p-1">
-                        <button type="button" onClick={() => selectTheme('light')} className="bg-white dark:bg-indigo-700 rounded-full h-3 w-3 md:h-4 md:w-4 focus:outline-none mr-1 "></button>
+                        <button type="button" onClick={() => selectTheme('light')} className="bg-white dark:bg-indigo-700 rounded-full h-3 w-3 md:h-4 md:w-4 focus:outline-none mr-2 "></button>
                         <button onClick={() => selectTheme('dark')} className="bg-indigo-400 dark:bg-white rounded-full h-3 w-3 md:h-4 md:w-4 focus:outline-none"></button>
                     </div>
                     <svg className="h-4 w-4 md:h-5 md:w-5 text-gray-500 dark:text-gray-50 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
