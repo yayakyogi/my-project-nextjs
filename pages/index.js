644282d738fa2f7df24b0feb4036d503782859f8
yayaktaka@gmail.com <yayaktaka@gmail.com>
2021-01-16 19:07:48 +0700

    update baru

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
+import Nav from '../components/Navbar'
+import Home from '../components/Home'
+import About from '../components/About'
+import Service from '../components/Service'
+import Footer from '../components/Footer'
+import Poortofolio from '../components/Portofolio'
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
-export default function Home() {
+export default function IndexPage() {
   return (
-    <div className="container bg-gray-100">
-      <Head>
+    <div id="top" className="antialias overflow-hidden dark:bg-gray-900">
+       <Head>
         <title>My Project</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="img/logo.png" />
       </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description text-2xl text-gray-700 font-extralight">
-          Hello World!
-        </p>
-      </main>
-
-      <Footer />
+      <Nav/>
+      <Home/>
+      <About/>
+      <Poortofolio/>
+      <Service/>
+      <Footer/>
+      <a href="#top" className="fixed bottom-5 rounded-sm right-4 bg-indigo-100 md:bg-indigo-800 p-2 focus:outline-none focus:ring-2 focus:ring-indigo-200">
+      <svg className="h-4 w-4 md:h-6 md:w-6 text-gray-600 md:text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 11l7-7 7 7M5 19l7-7 7 7" />
+      </svg>
+      </a>
     </div>
   )
 }
